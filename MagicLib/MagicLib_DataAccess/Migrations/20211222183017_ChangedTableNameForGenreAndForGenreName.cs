fee7ab8185c9d28223150b2396d206c2a8d00e0a
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-12-22 17:49:34 -0100

    Changed Genre table name and Genre column name with data annotations.

---
+++ MagicLib/MagicLib_DataAccess/Migrations/20211222183017_ChangedTableNameForGenreAndForGenreName.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MagicLib_DataAccess.Migrations
+{
+    public partial class ChangedTableNameForGenreAndForGenreName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Genres",
+                table: "Genres");
+
+            migrationBuilder.RenameTable(
+                name: "Genres",
+                newName: "Genre_table");
+
+            migrationBuilder.RenameColumn(
+                name: "GenreName",
+                table: "Genre_table",
+                newName: "Genre_name_column");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Genre_table",
+                table: "Genre_table",
+                column: "GenreId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Genre_table",
+                table: "Genre_table");
+
+            migrationBuilder.RenameTable(
+                name: "Genre_table",
+                newName: "Genres");
+
+            migrationBuilder.RenameColumn(
+                name: "Genre_name_column",
+                table: "Genres",
+                newName: "GenreName");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Genres",
+                table: "Genres",
+                column: "GenreId");
+        }
+    }
+}
--- MagicLib/MagicLib_Model/Models/Genre.cs
+++ MagicLib/MagicLib_Model/Models/Genre.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MagicLib_Model.Models
 {
+    [Table("Genre_table")]
     public class Genre
     {
+
         [Key]
         public int GenreId { get; set; }
+
+        [Column("Genre_name_column")]
         public string GenreName { get; set; }
        // public int DisplayOrder { get; set; }
         public int Quantity { get; set; }
