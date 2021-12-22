cf565a7a8a9dcc35f1e9e326ed3157b2f3dbac21
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-12-22 18:05:45 -0100

    Added required annotations, the key annotation isn't new to me.

--- MagicLib/MagicLib_DataAccess/Data/ApplicationDbContext.cs
+++ MagicLib/MagicLib_DataAccess/Data/ApplicationDbContext.cs
@@ -16,5 +16,6 @@ namespace MagicLib_DataAccess.Data
         }
        public DbSet<Category> Categories { get; set; } // This is how EF Core knows Category is a model. It creates it in DB if it doesn't exist
        public DbSet<Genre> Genres { get; set; } 
+       public DbSet<Book> Books { get; set; } 
     }
 }
--- MagicLib/MagicLib_DataAccess/Migrations/20211222185635_AddedBookModelToDb.Designer.cs
+++ MagicLib/MagicLib_DataAccess/Migrations/20211222185635_AddedBookModelToDb.Designer.cs
@@ -3,14 +3,16 @@ using MagicLib_DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MagicLib_DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211222185635_AddedBookModelToDb")]
+    partial class AddedBookModelToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -26,14 +28,12 @@ namespace MagicLib_DataAccess.Migrations
                         .UseIdentityColumn();
 
                     b.Property<string>("ISBN")
-                        .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<double>("Price")
                         .HasColumnType("float");
 
                     b.Property<string>("Title")
-                        .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.HasKey("Book_Id");
