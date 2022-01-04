b48e28e34674c063f0c5239b8be4d7873c81ba03
Pedro Sousa <pedro.p.sousa@sapo.pt>
2022-01-04 18:24:06 +0000

    Added CategoryController for category display, added a view template, display is working as intended. Debug Output window shows me the executed queries on the db by EFC.

---
+++ MagicLib/MagicLib/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using MagicLib_DataAccess.Data;
+using MagicLib_Model.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MagicLib.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db; // We can access _db to access our models and use EF to retrive, save, perform CRUD operations
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categoryList = _db.Categories.ToList();
+            return View(categoryList);
+        }
+    }
+}
