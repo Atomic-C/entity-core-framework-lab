ec4622425cd9822c25be713fa60f35cc839d77a4
Pedro Sousa <pedro.p.sousa@sapo.pt>
2022-01-04 17:51:44 +0000

    Added catgories to category table, but manually with a migration, enabled debug logging for queries on appsettings.

---
+++ MagicLib/MagicLib_DataAccess/Migrations/20220104173953_addRawCategoryToTable.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MagicLib_DataAccess.Migrations
+{
+    public partial class addRawCategoryToTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Fluent_Category VALUES ('Cat 1')");
+            migrationBuilder.Sql("INSERT INTO Fluent_Category VALUES ('Cat 2')");
+            migrationBuilder.Sql("INSERT INTO Fluent_Category VALUES ('Cat 3')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
