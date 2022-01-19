e5f80a67dfdee8f7d59f08c0551ee440f527ecc3
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2022-01-19 18:42:20 -0100

    Left Publisher and Author Controller skeletons are ready

--- MagicLib/MagicLib/Controllers/AuthorController.cs
+++ MagicLib/MagicLib/Controllers/AuthorController.cs
@@ -8,35 +8,42 @@ using System.Threading.Tasks;
 
 namespace MagicLib.Controllers
 {
-    public class PublisherController : Controller
+    public class AuthorController : Controller
     {
         private readonly ApplicationDbContext _db; // We need this when we pass data to our view,  DBContext!
-        public PublisherController(ApplicationDbContext db)
+        public AuthorController(ApplicationDbContext db)
         {
             _db = db;
         }
-        
+
         public IActionResult Index()
         {
+
+
             return View(); // pass the list to the view
         }
 
         // GET for Upsert
         public IActionResult Upsert(int? id) // This populates decides which view we get
         {
-                return View();
-        }       
+
+            return View();
+
+        }
         // POST for Upsert
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult UpsertPost(Publisher obj) // This method creates or updates Publishers
+        public IActionResult UpsertPost(Author obj) // This method creates or updates Authors
         {
-                return View();
+
+            return View();
+
         }
 
         public IActionResult Delete(int id)
         {
             return RedirectToAction(nameof(Index));
         }
+
     }
 }
