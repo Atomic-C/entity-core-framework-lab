7fd466be83207a750196b1b3a14d93a2e11f88e4
Pedro Sousa <pedro.p.sousa@sapo.pt>
2022-02-07 17:33:33 +0000

    Fixed AuthorBook model, forgot access modifier.

--- MagicLib/MagicLib_Model/Models/ViewModels/AuthorBookViewModel.cs
+++ MagicLib/MagicLib_Model/Models/ViewModels/AuthorBookViewModel.cs
@@ -9,10 +9,10 @@ namespace MagicLib_Model.Models.ViewModels
 {
     public class AuthorBookViewModel
     {
-        //public AuthorBook AuthorBook { get; set; } 
+        public AuthorBookMT AuthorBookMT { get; set; }
         public Book Book { get; set; }
 
-        //IEnumerable<AuthorBook> AuthorBookList { get; set; }
-        IEnumerable<SelectListItem> AuthorList { get; set; }
+        public IEnumerable<AuthorBookMT> AuthorBookList { get; set; } // To display all available authors for the book
+        public IEnumerable<SelectListItem> AuthorList { get; set; } // To display authors in a dropdown list
     }
 }
